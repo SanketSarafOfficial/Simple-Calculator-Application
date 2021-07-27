331ebb5be6bf06243b3444dd778f5956c0d171db
SanketSarafOfficial <62201648+SanketSarafOfficial@users.noreply.github.com>
2021-07-27 16:04:23 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,81 @@
+import React ,{useState} from 'react'
+import './calculator.css';
+
+
+function Calculator() {
+
+      const [result, setResult] = useState(" ");
+
+      const handleClick =(e)=>
+      {
+            setResult(result.concat(e.target.name));
+      }
+
+      const smash =()=>
+      {
+            setResult("");
+      }
+      const backspace =()=>
+      {
+            setResult(result.slice(0,-1));
+      }
+
+      const total=()=>
+      {
+            try{
+                  setResult(eval(result).toString());
+            }
+            catch(err)
+            {
+                  setResult("Error");
+            }
+      }
+
+      return (
+            <div className="fullbody">
+            
+            <div className="container">
+
+            <form>
+            <input type="text" value={result} />
+
+            </form>
+            </div>
+
+                  <div className="keypads">
+
+                  <button id="clear" onClick={smash}> Clear </button>
+                  <button id="backspace" onClick={backspace}> C </button>
+                  <button name="/" onClick={handleClick}> &divide; </button>
+                  <button name="7" onClick={handleClick}> 7 </button>
+                  <button name="8" onClick={handleClick}> 8 </button>
+                  <button name="9" onClick={handleClick}> 9 </button>
+                  <button name="*" onClick={handleClick}> &times;</button>
+                  <button name="4" onClick={handleClick}> 4 </button>
+                  <button name="5" onClick={handleClick}> 5 </button>
+                  <button name="6" onClick={handleClick}> 6 </button>
+                  <button name="-" onClick={handleClick}> &ndash;</button>
+                  <button name="1" onClick={handleClick}> 1 </button>
+                  <button name="2" onClick={handleClick}> 2 </button>
+                  <button name="3" onClick={handleClick}> 3 </button>
+                  <button name="+" onClick={handleClick}> + </button>
+                  <button name="0" onClick={handleClick}> 0 </button>
+                  <button name="." onClick={handleClick}> . </button>
+                  <button onClick={total}> = </button>
+                  
+                 
+
+
+
+                  </div>
+
+             </div>
+            
+      )
+}
+
+
+
+
+export default Calculator;
+
